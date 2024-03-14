17c3076756992d1fcb36a9536ece4a24b17899e3
emreylmzcom <delosemre@outlook.com>
2024-03-15 01:17:17 +0300

    main

--- tcore/urls.py
+++ tcore/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import IndexView, AboutView, ContactView
+from .views import IndexView, AboutView, ServiceView, ContactView, BlogView
+
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('about/', AboutView.as_view(), name='about'),
+    path('abouts/', AboutView.as_view(), name='abouts'),
+    path('service/', ServiceView.as_view(), name='service'),
+    path('blog/', BlogView.as_view(), name='blog'),
     path('contact/', ContactView.as_view(), name='contact'),
+
 ]
--- tcore/views.py
+++ tcore/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 class AboutView(TemplateView):
-    template_name = 'about.html'
+    template_name = 'abouts.html'
+
+class ServiceView(TemplateView):
+    template_name = 'service.html'
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
 
 class ContactView(TemplateView):
     template_name = 'contact.html'
+
+
+
+
+
+
